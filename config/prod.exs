cca8de5f48fe8d0ac1eead2c257c9e92eb2c2e74
Liam Brown <liamnb525@gmail.com>
2021-02-10 19:07:13 -0500

    ADS

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,6 @@ config :practice, PracticeWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :debug
+config :logger, level: :info
 
 import_config "prod.secret.exs"
