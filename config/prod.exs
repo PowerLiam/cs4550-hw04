8c57dc993a15aa93f509f5ef7845b3c3f24ab9e8
Liam Brown <liamnb525@gmail.com>
2021-02-10 19:00:02 -0500

    fdsa

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,6 @@ config :practice, PracticeWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 import_config "prod.secret.exs"
